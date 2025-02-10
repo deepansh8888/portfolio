e749bff4f6dfe0305b36e0a61c70e2571cdd90e6
Deepansh <deepansh8888@gmail.com>
2025-02-10 17:10:41 +0530

    added research section

--- src/Home.js
+++ src/Home.js
@@ -1,147 +1,91 @@
-import React from 'react';
+import React from "react";
+import {
+  experiences,
+  projects,
+  articles,
+  skills,
+  publications,
+  footerData,
+} from "./portfolioData";
 
 const Home = () => {
-  const projects = [
-    {
-      title: 'BlogWise',
-      description: 'A blogging platform for sharing insights and stories.',
-      techimages: [
-        'https://picsum.photos/seed/picsum/200',
-        'https://picsum.photos/seed/picsum/200'
-      ],
-      image: 'https://picsum.photos/seed/picsum/200',
-      livelink: 'https://blogwise-frontend.onrender.com/',
-      githublink: 'https://github.com/deepansh8888/blogwise',
-    },
-    {
-      title: 'KubeWise',
-      description: 'A Kubernetes management tool designed for developers.',
-      techimages: [
-        'https://picsum.photos/seed/picsum/200',
-        'https://picsum.photos/seed/picsum/200'
-      ],
-      image: 'https://picsum.photos/seed/picsum/200',
-      livelink: 'https://example.com/kubewise',
-      githublink: 'https://github.com/deepansh8888/OneBlood',
-    },
-    {
-      title: 'OneBlood',
-      description: 'A platform to connect blood donors with those in need.',
-      techimages: [
-        'https://picsum.photos/seed/picsum/200',
-        'https://picsum.photos/seed/picsum/200'
-      ],
-      image: 'https://picsum.photos/seed/picsum/200',
-      livelink: 'https://oneblood-frontend.onrender.com/',
-      githublink: 'https://github.com/deepansh8888/OneBlood',
-    }
-  ];
-
-  const articles = [
-    {   
-      title: 'How to Set Up Your Dev Environment', 
-      description: 'A comprehensive guide to creating an efficient development setup.'
-    },
-    { 
-      title: 'Voila: The Magic Behind Modern Web Apps', 
-      description: 'An exploration of cutting-edge web development techniques.'
-    },
-    { 
-      title: 'Lorem Ipsum and Its Origins', 
-      description: 'A look into the history and usage of placeholder text.'
-    },
-    { 
-      title: 'Ipsum: A Developer\'s Perspective', 
-      description: 'Insights on why we use dummy text in design and development.'
-    },
-    { 
-      title: 'Yeah, Code It!', 
-      description: 'Tips and tricks to improve your coding skills in today landscape.'
-    },
-    { 
-      title: 'Azure: Cloud Solutions Simplified', 
-      description: 'Understanding the basics of Azure cloud services and deployments.'
-    }
-  ];
-
-  const skills = [
-    {
-      title: 'languages',
-      images: ['/techImages/cpp.png', '/techImages/javascript.png', '/techImages/cLang.png'],
-    },{
-      title: 'frameworks',
-      images: ['/techImages/react.png', '/techImages/express.png' , '/techImages/node.png'],
-    },{
-      title: 'tools',
-      images: ['/techImages/azure.png', '/techImages/docker.png', '/techImages/kubernetes.png', '/techImages/git.png', '/techImages/vscode.png', '/techImages/github.png'],
-    },{
-      title: 'database',
-      images: ['/techImages/mysql.png', '/techImages/mongodb.png'],
-    },
-  ];
-
   return (
     <>
-      <div id="main-home-container">
+      <div id="about-main-div">
         <section id="about">
-          <img id='my-photo' src="/techImages/deepansh.jpg" alt="Profile Picture" />
-          <h1 id='main-name'>i'm deepansh kumar</h1>
-          <div id="about-text">
-            <span> full-stack developer 👨‍💻</span>
-            <span> cloud computing enthusiast ✨</span>
-            {/* <span>side project connoisseur 🥃</span>
-            <span>open source maintainer 🎥</span> */}
+          <img
+            id="about-photo"
+            src="/techImages/deepansh.jpg"
+            alt="Profile Picture"
+          />
+          <div>
+            <p id="about-name">i'm deepansh kumar</p>
+            <div id="about-content">
+              <span>full-stack developer 👨‍💻</span>
+              <span>cloud computing enthusiast ☁️</span>
+              <span>like to spend time cubing 🚀</span>
+              <span>✨✨</span>
+            </div>
           </div>
         </section>
+      </div>
 
+      <div id="main-home-container">
         {/* Timeline Section */}
-        <div id="timeline">
+        <div className="main-headings">
           <h1 className="thefont">timeline</h1>
-          <div id="iByte">
-            <h2>iByte Infomatics</h2>
-            <h3>Full Stack Web Developer</h3>
-            <p>
-              Working on exciting projects and building innovative solutions as
-              a Full Stack Web Developer at iByte Infomatics.
-            </p>
-          </div>
-          <div id="gfg">
-            <h2>GeeksForGeeks</h2>
-            <h3>Cloud Computing Article Writer</h3>
-            <p>
-              Creating engaging content and sharing insights as a tech-writer.
-            </p>
+        </div>
+        <div id="timeline">
+          <div className="experience">
+            {experiences.map((experience) => {
+              return (
+                <>
+                  <h2>{experience.company}</h2>
+                  <h3>{experience.profile}</h3>
+                  <p>{experience.date}</p>
+                  <p>{experience.content}</p>
+                </>
+              );
+            })}
           </div>
         </div>
 
-        <div id='skills'>
-        <h1 className="thefont">skills</h1>
+        {/* Skills Section */}
+        <div>
+          <div className="main-headings">
+            {" "}
+            <h1 className="thefont">skills</h1>
+          </div>
           <div>
-            {
-              skills.map((skillCategory)=>{
-                return (<div id='skill-card'>
+            {skills.map((skillCategory) => {
+              return (
+                <div id="skill-card">
                   <h2>{skillCategory.title}</h2>
-                  <div id='tech-images-div'>
-                    {
-                      skillCategory.images.map((img)=>{
-                        return (
-                          <>
-                          <img src={img} alt='tech-img' className='mini-images' />
-                          </>
-                        );
-                      })
-                    }
+                  <div id="tech-images-div">
+                    {skillCategory.images.map((img) => {
+                      return (
+                        <>
+                          <img
+                            src={img}
+                            alt="tech-img"
+                            className="mini-images"
+                          />
+                        </>
+                      );
+                    })}
                   </div>
-                </div>);
-              })
-            }
+                </div>
+              );
+            })}
           </div>
         </div>
-          
 
         {/* Projects Section */}
-        <div id="projects">
-        <h1 className="thefont">projects</h1>
+        <div>
+          <div className="main-headings">
+            <h1 className="thefont">projects</h1>
+          </div>
+
           {projects.map((project, index) => (
             <div id="project-card" key={index}>
               <h2>{project.title}</h2>
@@ -155,19 +99,21 @@ const Home = () => {
                 <img src={project.image} alt="project" />
               </div>
               <div className="project-links">
-                <a href={project.livelink}>
-                  Live  </a><br/><br/>
-                <a href={project.githublink}  >
-                  GitHub
-                </a>
+                <a href={project.livelink}>Live </a>
+                <br />
+                <br />
+                <a href={project.githublink}>GitHub</a>
               </div>
             </div>
           ))}
         </div>
 
         {/* Articles Section */}
-        <div id="articles">
-          <h1 className="thefont">articles</h1>
+        <div>
+          <div className="main-headings">
+            <h1 className="thefont">articles</h1>
+          </div>
+
           <div id="articlesbox">
             {articles.map((article, index) => (
               <div id="article-card" key={index}>
@@ -177,22 +123,37 @@ const Home = () => {
             ))}
           </div>
         </div>
+
+        {/* Publications Section */}
+        <div>
+          <div className="main-headings">
+            <h1 className="thefont">research</h1>
+          </div>
+          <div id="publicationsbox">
+            {publications.map((publication, index) => (
+              <a href={publication.link} className="full-div-links">
+                <div id="publication-card" key={index}>
+                  <h2>{publication.title}</h2>
+                  <h4>{publication.description}</h4>
+                </div>
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
 
       {/* Footer */}
       <div className="footer">
-        <a href="https://www.linkedin.com/in/deepansh8888">
-          <img src="/techImages/linkedinIcon.png" alt="linkedinLogo" />
-        </a>
-        <a href="https://github.com/deepansh8888">
-          <img src="/techImages/githubIcon.png" alt="githubLogo" />
-        </a>
-        <a href="mailto:deepansh8888@gmail.com">
-          <img src="/techImages/mailIcon.png" alt="gmailLogo" />
-        </a>
+        {footerData.map((data) => {
+          return (
+            <a href={data.href}>
+              <img src={data.src} alt={data.alt} />
+            </a>
+          );
+        })}
       </div>
     </>
   );
-}
+};
 
 export default Home;
---
+++ src/portfolioData.js
@@ -0,0 +1,125 @@
+
+
+
+export const experiences = [{
+    company: 'iByte Infomatics',
+    profile: 'Full Stack Web Developer',
+    date: 'x to x',
+    content: 'Working on exciting projects and building innovative solutions as a Full Stack Web Developer at iByte Infomatics.'
+},{
+    company: 'GeeksForGeeks',
+    profile: 'Technical Content Writer',
+    date: 'y to y',
+    content: ' Creating engaging content and sharing insights as a tech-writer.'
+}]; 
+
+export const  projects = [
+    {
+      title: 'BlogWise',
+      description: 'A blogging platform for sharing insights and stories.',
+      techimages: [
+        'https://picsum.photos/seed/picsum/200',
+        'https://picsum.photos/seed/picsum/200'
+      ],
+      image: 'https://picsum.photos/seed/picsum/200',
+      livelink: 'https://blogwise-frontend.onrender.com/',
+      githublink: 'https://github.com/deepansh8888/blogwise',
+    },
+    {
+      title: 'KubeWise',
+      description: 'A Kubernetes management tool designed for developers.',
+      techimages: [
+        'https://picsum.photos/seed/picsum/200',
+        'https://picsum.photos/seed/picsum/200'
+      ],
+      image: 'https://picsum.photos/seed/picsum/200',
+      livelink: 'https://example.com/kubewise',
+      githublink: 'https://github.com/deepansh8888/OneBlood',
+    },
+    {
+      title: 'OneBlood',
+      description: 'A platform to connect blood donors with those in need.',
+      techimages: [
+        'https://picsum.photos/seed/picsum/200',
+        'https://picsum.photos/seed/picsum/200'
+      ],
+      image: 'https://picsum.photos/seed/picsum/200',
+      livelink: 'https://oneblood-frontend.onrender.com/',
+      githublink: 'https://github.com/deepansh8888/OneBlood',
+    }
+  ];
+
+  export const articles = [
+    {   
+      title: 'How to Set Up Your Dev Environment', 
+      description: 'A comprehensive guide to creating an efficient development setup.'
+    },
+    { 
+      title: 'Voila: The Magic Behind Modern Web Apps', 
+      description: 'An exploration of cutting-edge web development techniques.'
+    },
+    { 
+      title: 'Lorem Ipsum and Its Origins', 
+      description: 'A look into the history and usage of placeholder text.'
+    },
+    { 
+      title: 'Ipsum: A Developer\'s Perspective', 
+      description: 'Insights on why we use dummy text in design and development.'
+    },
+    { 
+      title: 'Yeah, Code It!', 
+      description: 'Tips and tricks to improve your coding skills in today landscape.'
+    },
+    { 
+      title: 'Azure: Cloud Solutions Simplified', 
+      description: 'Understanding the basics of Azure cloud services and deployments.'
+    }
+  ];
+
+  export const skills = [
+    {
+      title: 'languages',
+      images: ['/techImages/cpp.png', '/techImages/javascript.png', '/techImages/cLang.png'],
+    },{
+      title: 'frameworks',
+      images: ['/techImages/react.png', '/techImages/express.png' , '/techImages/node.png'],
+    },{
+      title: 'tools',
+      images: ['/techImages/azure.png', '/techImages/docker.png', '/techImages/kubernetes.png', '/techImages/git.png', '/techImages/vscode.png', '/techImages/github.png'],
+    },{
+      title: 'database',
+      images: ['/techImages/mysql.png', '/techImages/mongodb.png'],
+    },
+  ];
+
+  export const publications = [{
+    title: 'Cloud Computing for IoT Sensing Data - Book Chapter',
+    description: 'Authored a chapter on Cloud Computing for IoT Sensing Data in collaboration with Dr. Ayushi Prakash',
+    link: 'https://www.igi-global.com/chapter/cloud-computing-for-iot-sensing-data/342143'
+  },{
+    title: 'Certificateless Multi-Replica Cloud Data Audit Scheme',
+    description: 'Researched an auditing scheme for disaster resilience systems to enhance data integrity and availability in cloud storage',
+    link: 'https://ieeexplore.ieee.org/document/10560092',
+  }];
+
+export const certifications = [{
+    title: 'Microsoft Certified: Azure AI Engineer Associate (AI-102)',
+    link: ''
+},{
+    title: 'Microsoft Azure Administrator (Koenig Solutions)',
+    link: ''
+}];
+
+export const footerData = [{
+    href: 'https://www.linkedin.com/in/deepansh8888',
+    src: '/techImages/linkedinIcon.png',
+    alt: 'linkedinLogo',
+},{
+    href: 'https://github.com/deepansh8888',
+    src: '/techImages/githubIcon.png',
+    alt: 'githubLogo',
+},{
+    href: 'mailto:deepansh8888@gmail.com',
+    src: '/techImages/mailIcon.png',
+    alt: 'mailLogo',
+}];
